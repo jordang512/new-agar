f3f1e9349e759fe6e9643b577e86324323372e6d
Isaac Pugh <spysick@gmail.com>
2015-06-03 09:19:21 -0700

    Commenting and Cleanup

--- main.js
+++ main.js
@@ -20,10 +20,13 @@ function drawBackground() {
 	ctx.fillRect(0, 0, c.width, c.height);
 	ctx.closePath();
 }
+
+// Sets the speed of given user
 function setSpeed(user) {
-// Speed is a logarithmic curve
-user.speed = Math.pow(Math.sqrt(player.size+190),-1)*4000*speedMult;
+	user.speed = Math.pow(Math.sqrt(player.size+190),-1)*4000*speedMult;
 }
+
+// Draws given user on the canvas
 function drawUser(user) {
 	ctx.beginPath();
 	ctx.fillStyle = user.color;
@@ -49,17 +52,21 @@ function drawUser(user) {
 						pellets.splice(i, 1);
 						setSpeed(user);
 					} else if (user.style == "circle") {
-						if (Math.sqrt((pellets[i].location.y-user.location.y)*(pellets[i].location.y-user.location.y)+(pellets[i].location.x-user.location.x)*(pellets[i].location.x-user.location.x))<radius) {user.size += pellets[i].size;
+						if (Math.sqrt((pellets[i].location.y-user.location.y)*(pellets[i].location.y-user.location.y)+(pellets[i].location.x-user.location.x)*(pellets[i].location.x-user.location.x))<radius) {
+							user.size += pellets[i].size;
 							pellets.splice(i, 1);
-							setSpeed(user);}
+							setSpeed(user);
 						}
 					}
 				}
 			}
-//drawScore();
+		}
+	}
 }
-}
+
+// Moves the player based on the mouse coords
 function moveUser() {
+	// Calculates the distance between the player and mouse
 	var distX = Math.abs(c.width/2 - MouseX);
 	var distY = Math.abs(c.height/2 - MouseY);
 	var dX = distX;
@@ -67,12 +74,15 @@ function moveUser() {
 	distX /= Math.sqrt((dX * dX) + (dY * dY));
 	distY /= Math.sqrt((dX * dX) + (dY * dY));
 	radius = Math.sqrt(player.size/Math.PI) * 10;
+	// Gets slower as the mouse gets closer
 	if (MouseX > c.width/2 - 120 && MouseX < c.width/2 + 120) {
 		distX /= 120/ Math.abs(MouseX - c.width/2);
 	}
 	if (MouseY > c.height/2 - 120 && MouseY < c.height/2 + 120) {
 		distY /= 120/ Math.abs(MouseY - c.height/2);
 	}
+	// Stops moving if mouse is within 10px of the center of the player
+	// Moves player towards mouse
 	if(!(MouseX > c.width/2 - 10 && MouseX < c.width/2 + 10 && MouseY > c.height/2 && MouseY < c.height/2 + 20)) {
 		if (player.location.x > 0 && MouseX < c.width/2) {
 			player.location.x -= player.speed/fps * distX;
@@ -86,29 +96,40 @@ function moveUser() {
 		}
 	}
 }
+
+// Uses JQuery and html to find mouse coords
 function setMousePosition(e) {
 	MouseX = e.clientX - ($(window).width()/2 - c.width/2)+160;
 	MouseY = e.clientY;
 }
+
+// Makes a random pellet on the screen
 function makePellet() {
 	var pX = Math.floor((Math.random()) * size.width);
 	var pY = Math.floor((Math.random()) * size.height);
 	var pellet = {location:{x: pX, y: pY}, size:1, type: "pellet", color:randRGB(), style: "circle"};
 	pellets.push(pellet);
 }
+
+// Creates a random color for the pellets
 function randRGB() {
 	var r = Math.floor(Math.random()*255);
 	var g = Math.floor(Math.random()*255);
 	var b = Math.floor(Math.random()*255);
 	return "rgb("+r+","+g+","+b+")";
 }
+
+// Makes 500 pellets at the start of the game
 function makeGamePellets() {
 	for (i = 0; i < 500; i++) {
 		makePellet();
 	}
 }
+
+var maxPellets = 2000;
+// Makes a pellet every frame, maxing out at 2000
 function makePellets() {
-	if (pellets.length < 2000) {
+	if (pellets.length < maxPellets) {
 		if (pCount == Math.floor(0)){
 			pCount = 0;
 			makePellet();
@@ -117,6 +138,8 @@ function makePellets() {
 		}
 	}
 }
+
+// Draws all the pellets around the player
 function drawPellets() {
 	for(i in pellets) {
 		if (!(pellets[i].location.x > player.location.x + c.height/2 && pellets[i].location.x < player.location.x - c.height/2)) {
@@ -126,32 +149,39 @@ function drawPellets() {
 		}
 	}
 }
+
+// Show's the player's mass in the bottom left corner of the canvas
 function drawScore() {
 	ctx.beginPath();
 	ctx.fillStyle = "gray";
 	ctx.font = "22px Arial";
-	ctx.fillText("Score: " + player.size + ", Speed: " + Math.floor(player.speed), 20, c.height - 25);
+	ctx.fillText("Size: " + player.size, 20, c.height - 25);
 }
+
+var pButtonLoc = {x: 540, y: 540};
+//Draws the pause button
 function drawPauseSymbol() {
 	ctx.beginPath();
-	ctx.moveTo(c.width/2 - 30, c.height/2 - 40);
-	ctx.lineTo(c.width/2 + 30, c.height/2 - 40);
-	ctx.quadraticCurveTo(c.width/2 + 40, c.height/2 - 40, c.width/2 + 40, c.height/2 - 30);
-	ctx.lineTo(c.width/2 + 40, c.height/2 + 30);
-	ctx.quadraticCurveTo(c.width/2 + 40, c.height/2 + 40, c.width/2 + 30, c.height/2 + 40);
-	ctx.lineTo(c.width/2 - 30, c.height/2 + 40);
-	ctx.quadraticCurveTo(c.width/2 - 40, c.height/2 + 40, c.width/2 - 40, c.height/2 + 30);
-	ctx.lineTo(c.width/2 - 40, c.height/2 - 30);
-	ctx.quadraticCurveTo(c.width/2 - 40, c.height/2 - 40, c.width/2 - 30, c.height/2 - 40);
+	ctx.moveTo(pButtonLoc.x - 30, pButtonLoc.y - 40);
+	ctx.lineTo(pButtonLoc.x + 30, pButtonLoc.y - 40);
+	ctx.quadraticCurveTo(pButtonLoc.x + 40, pButtonLoc.y - 40, pButtonLoc.x + 40, pButtonLoc.y - 30);
+	ctx.lineTo(pButtonLoc.x + 40, pButtonLoc.y + 30);
+	ctx.quadraticCurveTo(pButtonLoc.x + 40, pButtonLoc.y + 40, pButtonLoc.x + 30, pButtonLoc.y + 40);
+	ctx.lineTo(pButtonLoc.x - 30, pButtonLoc.y + 40);
+	ctx.quadraticCurveTo(pButtonLoc.x - 40, pButtonLoc.y + 40, pButtonLoc.x - 40, pButtonLoc.y + 30);
+	ctx.lineTo(pButtonLoc.x - 40, pButtonLoc.y - 30);
+	ctx.quadraticCurveTo(pButtonLoc.x - 40, pButtonLoc.y - 40, pButtonLoc.x - 30, pButtonLoc.y - 40);
 	ctx.fillStyle = "gold";
 	ctx.fill();
 	ctx.fillStyle = "silver";
-	ctx.fillRect(c.width/2 - 27, c.height/2 - 30, 20, 60);
-	ctx.fillRect(c.width/2 + 7, c.height/2 - 30, 20, 60);
-	ctx.rect(c.width/2 - 27, c.height/2 - 30, 20, 60);
-	ctx.rect(c.width/2 + 7, c.height/2 - 30, 20, 60);
+	ctx.fillRect(pButtonLoc.x - 27, pButtonLoc.y - 30, 20, 60);
+	ctx.fillRect(pButtonLoc.x + 7, pButtonLoc.y - 30, 20, 60);
+	ctx.rect(pButtonLoc.x - 27, pButtonLoc.y - 30, 20, 60);
+	ctx.rect(pButtonLoc.x + 7, pButtonLoc.y - 30, 20, 60);
 	ctx.stroke();
 }
+
+// The function calls that happen every frame
 function intervalFunctionCalls() {
 	drawBackground();
 	if(!paused) {
@@ -166,6 +196,8 @@ function intervalFunctionCalls() {
 		drawPauseSymbol();
 	}
 }
+
+// Attempts to increase the player's speed. Only increases if they have 10 more mass than the cost
 function increaseSpeed() {
 	if (player.size > 10 + speedIncCost) {
 		speedMult += 0.05;
@@ -177,6 +209,24 @@ function increaseSpeed() {
 		addNotification("You need to have at least 10 mass left after the purchase!", fps*5, 20, 25);
 	}
 }
+
+// Setter for player.style
+function setPlayerStyle(style) {
+	player.style = style;
+}
+
+// Setter for player.color
+function setColor(color) {
+	player.color = color;
+}
+
+// adds a notification to the notification array
+function addNotification(message, ticks, x, y) {
+	notification = {message: message, ticks: ticks, location:{x: x, y: y}};
+	notifications.push(notification);
+}
+
+// Displays a notification
 function notify() {
 	ctx.beginPath();
 	for (i in notifications) {
@@ -189,17 +239,10 @@ function notify() {
 		}
 	}
 }
-function setPlayerStyle(style) {
-	player.style = style;
-}
-function setColor(color) {
-	player.color = color;
-}
-function addNotification(message, ticks, x, y) {
-	notification = {message: message, ticks: ticks, location:{x: x, y: y}};
-	notifications.push(notification);
-}
+
+// Listens for keypresses
 document.addEventListener('keydown', function(event) {
+	// p
 	if(event.keyCode == 80) {
 		if(paused) {
 			paused = false;
@@ -207,16 +250,20 @@ document.addEventListener('keydown', function(event) {
 			paused = true;
 		}
 	}
+	// i
 	if(event.keyCode == 73) {
 		increaseSpeed();
 	}
+	// 1
 	if(event.keyCode == 49) {
 		setPlayerStyle('circle');
 	}
+	// 2
 	if(event.keyCode == 50) {
 		setPlayerStyle('square');
 	}
 });
+
 setSpeed(player);
 makeGamePellets();
 setInterval(function() {intervalFunctionCalls();}, 1000/fps);
